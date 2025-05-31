0b429fbcf002e9252d7389212cc4a4ee6a060d77
lemonlemonde <mirudjun@gmail.com>
2025-05-31 15:35:11 -0700

    :sparkles: Better resolution my eyes hurt

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 rm -f /tmp/.X1-lock /tmp/.X11-unix/X1
 
 # Xvfb, make virtual display :1
-Xvfb :1 -screen 0 1280x800x24 > /tmp/xvfb.log 2>&1 &
+Xvfb :1 -screen 0 1920x1080x24 > /tmp/xvfb.log 2>&1 &
 sleep 5
 
 # start desktop (xfce4) on same display (:1)
@@ -14,9 +14,6 @@ sleep 5
 # x11vnc server
 pkill x11vnc || true
 x11vnc -display :1 -nopw -forever > /tmp/x11vnc.log 2>&1 &
-
-# start gazebo by default
-gz sim > /tmp/gazebo.log 2>&1 &
 
 # keep alive
 tail -f /dev/null
